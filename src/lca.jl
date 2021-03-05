602150cd6e14afa05dc1ff1fbb9bfa4ecbf20402
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-03-05 18:48:47 +0900

    code LCA()

---
+++ src/lca.jl
@@ -0,0 +1,29 @@
+function LCA(leaves::Dict{Taxon,Int},cutoff::Float64)
+    @assert cutoff > 0.5 && cutoff < 1
+
+    taxa = collect(keys(leaves))
+    tree = topolgoy(taxa)
+
+    total_bitscore = sum(values(leaves))
+    threshold_bitscore = cutoff * total_bitscore
+
+    next = Stack{PhyloTree}(10)
+    push!(next,tree)
+    current_lca = tree
+    while true
+        current_node = pop!(next)
+        sub_bitscore = bitscore(current_node,leaves)
+        if sub_bitscore >= threshold_bitscore
+            current_lca = current_node
+            children_tree =  current_node.children
+            for ctree in children_tree; push!(next,ctree); end
+        end
+        if isempty(next)
+            return current_lca.node
+        end
+    end
+end
+
+function bitscore(tree::PhyloTree,leaves::Dict{Taxon,Int})
+     return sum([leaves[leave.node] for leave in Leaves(tree)])
+end
