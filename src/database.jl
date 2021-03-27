3269ce4a9d4d63753efcd69063cd00cea8089b59
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-03-27 09:00:28 +0900

    add transaction(code faster!)

--- src/database.jl
+++ src/database.jl
@@ -2,7 +2,7 @@ const table="accession2taxid"
 
 function create!(db::SQLite.DB, source::String; chunk::Int=400, overwrite::Bool=false, header::Bool=true, accession_col::Int=2, taxid_col::Int=3)
     overwrite ? DBInterface.execute(db,"DROP TABLE IF EXISTS $table") : nothing
-    DBInterface.execute(db,"CREATE TABLE $table(accession TEXT PRIMARY KEY, taxid INTEGER)")
+    DBInterface.execute(db,"CREATE TABLE $table(accession TEXT, taxid INTEGER)")
     insert!(db, source; chunk=chunk, header=header, accession_col=accession_col, taxid_col=taxid_col)
     return db
 end
@@ -15,13 +15,18 @@ function Base.insert!(db::SQLite.DB, source::String; chunk::Int=400, header::Boo
 end
 
 function _insert_rows!(db::SQLite.DB, f::IOStream, chunk::Int, accession_col::Int, taxid_col::Int)
-    while !eof(f)
-        chunks = [readline(f) for _ in 1:chunk if !eof(f)]
-        cols = map( x-> split(x, "\t"), chunks)
-        accessions = map(x -> getindex(x,accession_col), cols)
-        taxids = map(x -> getindex(x,taxid_col), cols)
-        stmt = DBInterface.prepare(db, "INSERT INTO $table(accession, taxid) VALUES(:accession,:taxid)")
-        DBInterface.executemany(stmt, (accession=accessions, taxid=taxids))
+    SQLite.transaction(db)
+    try
+        while !eof(f)
+            chunks = [readline(f) for _ in 1:chunk if !eof(f)]
+            cols = map( x-> split(x, "\t"), chunks)
+            accessions = map(x -> getindex(x,accession_col), cols)
+            taxids = map(x -> getindex(x,taxid_col), cols)
+            stmt = DBInterface.prepare(db, "INSERT INTO $table(accession, taxid) VALUES(:accession,:taxid)")
+            DBInterface.executemany(stmt, (accession=accessions, taxid=taxids))
+        end
+    finally
+        SQLite.commit(db)
     end
 end
 
