a9221c76d46b48e9ec305eb23a3a4297c8fac77e
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-03-28 15:01:24 +0900

    struct BlastResult

--- src/lca.jl
+++ src/lca.jl
@@ -2,22 +2,22 @@ function BestHit(leaves::Dict{Taxon,Int})
     return last(findmax(leaves))
 end
 
-function LCA(leaves::Dict{Taxon,Int},minimal::Float64)
-    besthitscore = first(findmax(leaves))
-    filter!(x -> last(x) < besthitscore*minimal, leaves)
+function LCA(leaves::Dict{Taxon,BlastResult},minimal::Float64)
+    besthitscore = first(findmax([last(l).bitscore for l in leaves]))
+    filter!(x -> last(x).bitscore < besthitscore*minimal, leaves)
     taxa = collect(keys(leaves))
     return lca_node = lca(taxa)
 end
 
 function weightedLCA(leaves::Dict{Taxon,Int}, minimal::Float64, cutoff::Float64)
     @assert cutoff > 0.5 && cutoff < 1
-    besthitscore = first(findmax(leaves))
-    filter!(x -> last(x) < besthitscore*minimal, leaves)
+    besthitscore = first(findmax([last(l).bitscore for l in leaves]))
+    filter!(x -> last(x).bitscore < besthitscore*minimal, leaves)
 
     taxa = collect(keys(leaves))
     tree = topolgoy(taxa)
 
-    total_bitscore = sum(values(leaves))
+    total_bitscore = sum([last(l).bitscore for l in leaves])
     threshold_bitscore = cutoff * total_bitscore
 
     next = Stack{PhyloTree}()
@@ -25,14 +25,15 @@ function weightedLCA(leaves::Dict{Taxon,Int}, minimal::Float64, cutoff::Float64)
     current_lca = tree
     while true
         current_node = pop!(next)
-        sub_bitscore = sum([leaves[leave.node] for leave in Leaves(tree)])
+        sub_bitscore = sum([leaves[leave.node].bitscore for leave in Leaves(tree)])
         if sub_bitscore >= threshold_bitscore
             current_lca = current_node
             children_tree =  current_node.children
             for ctree in children_tree; push!(next,ctree); end
         end
         if isempty(next)
-            return current_lca.node
+            break
         end
     end
+    return current_lca.node
 end
--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,25 @@
+struct BlastResult 
+    qseqid::String
+    sseqid::String
+    pident::Float64
+    length::Int
+    mismatch::Int
+    gapopen::Int
+    qstart::Int
+    qend::Int
+    sstrat::Int
+    send::Int
+    evalue::Float64
+    bitscore::Int
+end
+
+
+function BlastResult(line::AbstractString)
+    cols = split(line, "\t")
+    @assert length(cols) == 12
+    return BlastResult(cols)
+end
+
 function blastLCA(;filepath::AbstractString, outpath::AbstractString, sqlite::SQLite.DB, taxonomy::Taxonomy.DB, method::Function, header::Bool=false)
     f = open(filepath,"r")
     o = open(outpath,"w")
@@ -9,38 +31,33 @@ end
 function blastLCA(f::IOStream, o::IOStream; sqlite::SQLite.DB, taxonomy::Taxonomy.DB, method::Function, header::Bool=false)
     header ? readline(f) : nothing #skip header
 
-    bitscores = Dict{Taxon,Int}()
+    results = Dict{Taxon,BlastResult}()
 
     line = readline(f) #read first line
+    record = BlastResult(line)
     while true
-        rows = split(line, "\t")    
-        @assert length(rows) == 12
-        qseqid = rows[1]
-        sseqid = rows[2]
-        bitscore = rows[12]
-        taxid = get(sqlite, sseqid, nothing)
+        taxid = get(sqlite, record.sseqid, nothing)
         @assert taxid !== nothing
 
         taxon = Taxon(taxid,taxonomy)
-        if ! haskey(bitscores, taxon)
-            bitscores[taxon] = bitscore
+        if ! haskey(results, taxon)
+            results[taxon] = record
         else
-            if bitscore > bitscores[taxon]
-                bitscores[taxon] = bitscore
+            if record.bitscore > results[taxon].bitscores
+                results[taxon] = record
             end
         end
 
         next = readline(f)
-        next_rows = split(line, "\t")
-        next_qseqid = next_rows[1]
+        next_record = BlastResult(next)
 
-        if qseqid != next_qseqid
-            assignment = method(bitscores)
+        if record.qseqid != next_record.qseqid
+            assignment = method(results)
             write(o,"$(qseqid)\t$(assignment)")
-            bitscores = Dict{Taxon,Int}() #initialize
+            results = Dict{Taxon,BlastResult}() #initialize
         end
 
-        line = next
+        record = next_record
         
         eof(f) ? break : nothing
     end
