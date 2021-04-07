67ac778ffaa32dc48875005d4abf643408035b73
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-04-07 11:16:49 +0900

    fix typo

--- src/main.jl
+++ src/main.jl
@@ -77,7 +77,7 @@ function blastLCA(f::IOStream, o::IOStream; sqlite::SQLite.DB, taxonomy::Taxonom
         if record.qseqid != next_qseqid  
             lineage = method(results)
             lineage_txt = lineage_line(lineage)
-            write(o,"$(qseqid)\t$(lineage_txt)")
+            write(o,"$(qseqid)\t$(lineage_txt)\n")
             results = Dict{Taxon,BlastResult}() #initialize
         end
         
