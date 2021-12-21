c4a3b184af590858a9607e20998c80e338186813
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-12-21 10:37:38 +0900

    made the main process run asynchronously.

--- src/main.jl
+++ src/main.jl
@@ -33,6 +33,7 @@ function BlastResult(line::AbstractString)
     return BlastResult(qseqid, sseqid, pident, length, mismatch, gapopen, qstart, qend, sstart, send, evalue, bitscore)
 end
 
+qseqid(record::BlastResult) = record.qseqid
 sseqid(record::BlastResult) = record.sseqid
 
 function blastLCA(;filepath::AbstractString, outpath::AbstractString, sqlite::SQLite.DB, taxonomy::Taxonomy.DB, method::Function, header::Bool=false, ranks=[:superkingdom, :phylum, :class, :order, :family, :genus, :species]) 
@@ -47,21 +48,44 @@ function blastLCA(;filepath::AbstractString, outpath::AbstractString, sqlite::SQ
 end
 
 function blastLCA(f::IOStream, o::IOStream; sqlite::SQLite.DB, taxonomy::Taxonomy.DB, method::Function, header::Bool=false, ranks=[:superkingdom, :phylum, :class, :order, :family, :genus, :species])
-    header ? readline(f) : nothing #skip header
+
+    blastresult_ch = Channel{BlastResult}(32)
+    lcainput_ch = Channel{Tuple{String,Dict{Taxon,BlastResult}}}(32)
+    lineage_ch = Channel{Tuple{String,Lineage}}(32)
+
+    @async parse_blastresult!(blastresult_ch, f, header)
+    @async put_blastresults!(lcainput_ch, blastresult_ch, sqlite, taxonomy)
+    @async lca_blastresults!(lineage_ch, lcainput_ch, method, ranks)
+
+    for (qseqid, lineage) in lineage_ch
+        lineage_txt = sprint(io -> print_lineage(io, lineage))
+        write(o, "$(qseqid)\t$(lineage_txt)\n")
+    end
+end
+
+function parse_blastresult!(out_channel::Channel{BlastResult}, f::IOStream, header::Bool)
+    header ? readline(f) : nothing
+    for line in eachline(f)
+        record = BlastResult(line)
+        put!(out_channel, record)
+    end
+    close(out_channel)
+end
+
+function put_blastresults!(out_channel::Channel{Tuple{String,Dict{Taxon,BlastResult}}}, in_channel::Channel{BlastResult}, sqlite::SQLite.DB, taxonomy::Taxonomy.DB)
 
     results = Dict{Taxon,BlastResult}()
 
-    line = readline(f) #read first line
     while true
-        record = BlastResult(line)
+        record = take!(in_channel)
         taxid = get(sqlite, sseqid(record), nothing)
+
         if taxid === nothing
             @warn "record $(sseqid(record)) has no taxid in $(sqlite.file)"
             taxon = nothing
         else
             taxon = get(taxid, taxonomy, nothing)
         end
-
 
         if taxon === nothing
             @warn "There is no taxon correspondinig to $(taxid)!\ncontinue..."
@@ -75,16 +99,25 @@ function blastLCA(f::IOStream, o::IOStream; sqlite::SQLite.DB, taxonomy::Taxonom
             end
         end
 
-        next = readline(f)
-        next_qseqid = split(next, "\t")[1]
-        if record.qseqid != next_qseqid && !isempty(results)
-            taxon = method(results)
-            lineage = reformat(Lineage(taxon), ranks)
-            lineage_txt = sprint(io -> print_lineage(io, lineage))
-            write(o,"$(record.qseqid)\t$(lineage_txt)\n")
-            results = Dict{Taxon,BlastResult}() #initialize
+        try
+            next = fetch(in_channel)
+            if qseqid(record) != qseqid(next)
+                put!(out_channel, (qseqid(record),results))
+                results = Dict{Taxon, BlastResult}()
+            end
+        catch e
+            put!(out_channel, (qseqid(record),results))
+            break
         end
-        
-        isempty(next) ? break : line = next
     end
+    close(out_channel)
+end
+
+function lca_blastresults!(out_channel::Channel{Tuple{String, Lineage}}, in_channel::Channel{Tuple{String, Dict{Taxon, BlastResult}}}, method::Function, ranks)
+    for results in in_channel
+        taxon = method(results[2])
+        lineage = reformat(Lineage(taxon), ranks)
+        put!(out_channel, (results[1], lineage))
+    end
+    close(out_channel)
 end
