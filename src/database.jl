cd7de376f144b61333137d7ce42919b6b4aa3966
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-12-28 13:18:21 +0900

    remove chunks

--- src/database.jl
+++ src/database.jl
@@ -1,28 +1,26 @@
 const table="accession2taxid"
 
-function create!(db::SQLite.DB, source::String; chunk::Int=400, overwrite::Bool=false, header::Bool=true, accession_col::Int=2, taxid_col::Int=3)
+function create!(db::SQLite.DB, source::String; overwrite::Bool=false, header::Bool=true, accession_col::Int=2, taxid_col::Int=3)
     overwrite ? DBInterface.execute(db,"DROP TABLE IF EXISTS $table") : nothing
     DBInterface.execute(db,"CREATE TABLE $table(accession TEXT PRIMARY KEY, taxid INTEGER)")
-    insert!(db, source; chunk=chunk, header=header, accession_col=accession_col, taxid_col=taxid_col)
+    insert!(db, source; header=header, accession_col=accession_col, taxid_col=taxid_col)
     return db
 end
 
-function Base.insert!(db::SQLite.DB, source::String; chunk::Int=400, header::Bool=true, accession_col::Int=2, taxid_col::Int=3)
+function Base.insert!(db::SQLite.DB, source::String; header::Bool=true, accession_col::Int=2, taxid_col::Int=3)
     f = open(source, "r")
     header ? readline(f) : nothing #skip header
-    _insert_rows!(db, f, chunk, accession_col, taxid_col)
+    _insert_rows!(db, f, accession_col, taxid_col)
     close(f)
 end
 
-function _insert_rows!(db::SQLite.DB, f::IOStream, chunk::Int, accession_col::Int, taxid_col::Int)
-    while !eof(f)
-        chunks = [readline(f) for _ in 1:chunk if !eof(f)]
-        cols = map( x-> split(x, "\t"), chunks)
-        accessions = map(x -> getindex(x,accession_col), cols)
-        taxids = map(x -> getindex(x,taxid_col), cols) 
-        stmt = DBInterface.prepare(db, "INSERT INTO $table(accession, taxid) VALUES(:accession,:taxid)")
-        DBInterface.executemany(stmt, (accession=accessions, taxid=taxids))
-    end
+function _insert_rows!(db::SQLite.DB, f::IOStream, accession_col::Int, taxid_col::Int)
+    lines = [line for line in readlines(f)]
+    cols = map( x-> split(x, "\t"), chunks)
+    accessions = map(x -> getindex(x,accession_col), cols)
+    taxids = map(x -> getindex(x,taxid_col), cols) 
+    stmt = DBInterface.prepare(db, "INSERT INTO $table(accession, taxid) VALUES(:accession,:taxid)")
+    DBInterface.executemany(stmt, (accession=accessions, taxid=taxids))
 end
 
 function Base.get(db::SQLite.DB, accession::String, default::Any)
