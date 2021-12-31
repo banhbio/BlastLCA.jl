c9e5febab78becb2bcfad132e30e7b6b8f5cdcd6
banhbio <ban@kuicr.kyoto-u.ac.jp>
2022-01-01 00:07:28 +0900

    fix

--- src/database.jl
+++ src/database.jl
@@ -3,7 +3,7 @@ const table="accession2taxid"
 function create!(db::SQLite.DB, source::String; overwrite::Bool=false, header::Bool=true, delim::Union{Nothing, Char, String}=nothing , accession_col::Int=2, taxid_col::Int=3)
     overwrite ? DBInterface.execute(db,"DROP TABLE IF EXISTS $table") : nothing
     col_width = max(accession_col, taxid_col)
-    temp_colname= ["Column_$i "for i in 1:col_width]
+    temp_colname= [ "Column_$i" for i in 1:col_width ]
     temp_colname[accession_col] = "accession"
     temp_colname[taxid_col] = "taxid"
     CSV.File(source; header=temp_colname, select=[accession_col,taxid_col], delim=delim) |> SQLite.load!(db, table)
