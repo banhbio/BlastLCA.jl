f37e4c09c263c3917918a2c973231567ee3de1d3
banhbio <ban@kuicr.kyoto-u.ac.jp>
2022-12-26 10:52:17 +0900

    fix export

--- src/BlastLCA.jl
+++ src/BlastLCA.jl
@@ -8,12 +8,10 @@ using DataStructures:
     Stack
 using DataFrames
 
-export create!,insert!,get,
-       BlastResult,
+export BlastResult,
        blastLCA,
        BestHit,freeLCA,weightedLCA
 include("main.jl")
 include("tree.jl")
 include("lca.jl")
-include("database.jl")
 end
